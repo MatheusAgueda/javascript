5f513eb15aa7d31904886b41a6f8bdb57ce1c448
Matheus Henrique <105541655+MatheusAgueda@users.noreply.github.com>
2023-03-30 12:48:10 +0100

    1

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,48 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play()
+}
+
+function tocaSomClap (){
+    document.querySelector('#som_tecla_clap').play()
+}
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play()
+}
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play()
+}
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play()
+}
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play()
+}
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play ()
+}
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play ()
+}
+
+document.querySelector('.tecla_pom')
+
+document.querySelector('.tecla_clap')
+
+document.querySelector('.tecla_puff')
+
+document.querySelector('.tecla_splash')
+
+document.querySelector('.tecla_toim')
+
+document.querySelector('.tecla_psh')
+
+document.querySelector('.tecla_tic')
+
+document.querySelector('.tecla_tom')
+
